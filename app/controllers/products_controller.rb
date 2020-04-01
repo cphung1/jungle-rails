4d3bef55c703234144bdad6916cc41d81e0ea7e3
cphung1 <cphung97@gmail.com>
2020-04-01 20:08:01 +0000

    comments for clarity

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
+    # Uses MODEL 
     @products = Product.all.order(created_at: :desc)
+
+    # VIEW
+    render :index #this doesn't need to be included is implicit from line above, but added for clarity /views/products/index.html.erb
   end
 
   def show
