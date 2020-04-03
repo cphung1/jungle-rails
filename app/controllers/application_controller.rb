abc6c3542dc2f09c7bd80c5bb768f1b9f6683c65
cphung1 <cphung97@gmail.com>
2020-04-03 00:19:52 +0000

    user sign up, log in and log out basic functionality works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,13 +20,23 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart_subtotal_cents
 
-
   def update_cart(new_cart)
     cookies[:cart] = {
       value: JSON.generate(new_cart),
       expires: 10.days.from_now
     }
     cookies[:cart]
+  end
+
+  protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
   end
 
 end
