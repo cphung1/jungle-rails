a30331645e3ca2f7f8e89a504a8dfb181f1f98fd
cphung1 <cphung97@gmail.com>
2020-04-03 00:32:52 +0000

    fixed sign up form password field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,8 +28,6 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
-  protect_from_forgery with: :exception
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-
-
   validates :name, presence: true
   validates :email, presence: true, :format => /@/
-  validates :password, presence: true
   validates :password_confirmation, presence: true
 end
